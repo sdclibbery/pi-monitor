4b967242a1de8f0b2055b64178f3ef78e8c65d53
Stephen Clibbery <sdclibbery@googlemail.com>
2018-03-04 22:29:47 +0000

    localtunnel subdomain from hostname

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const localtunnel = require('localtunnel')
+const os =  require('os')
 
 const port = 8000
+const tunnelName =  os.hostname().toLowerCase().replace(/[^a-z0-9]/g, '')
+const localtunnelSubdomain = `sdclibbery${tunnelName}`
+console.log(localtunnelSubdomain)
 const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => {
   console.log('pi-monitor listening on port '+port)
-  localtunnel(port, { subdomain: 'sdclibberytradrpi' }, (err, tunnel) => {
-    console.log(err)
-    console.log(tunnel.url)
+  localtunnel(port, { subdomain: localtunnelSubdomain }, (err, tunnel) => {
+    console.log('localtunnel: ', err || (tunnel && tunnel.url))
   })
 })
