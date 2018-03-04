6ebb741c45f08a90e420b5c572167d0e07ecccaa
Stephen Clibbery <sdclibbery@googlemail.com>
2018-03-04 22:58:58 +0000

    basic monitoring stats

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ToDo
-* install https://github.com/localtunnel/localtunnel and run from inside THIS app...
- * Use machine hostname for subdomain (with 'sdclibbery' prefix)
-* Simple page that gets the pi CPU load, memory stats, heap status, disk space etc
+x install https://github.com/localtunnel/localtunnel and run from inside THIS app...
+ x Use machine hostname for subdomain (with 'sdclibbery' prefix)
+x Simple page that gets the pi CPU load, memory stats, heap status, disk space etc
 * Pages for running and monitoring bots
--- index.js
+++ index.js
@@ -7,11 +7,22 @@ const tunnelName =  os.hostname().toLowerCase().replace(/[^a-z0-9]/g, '')
 const localtunnelSubdomain = `sdclibbery${tunnelName}`
 console.log(localtunnelSubdomain)
 const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => {
   console.log('pi-monitor listening on port '+port)
   localtunnel(port, { subdomain: localtunnelSubdomain }, (err, tunnel) => {
     console.log('localtunnel: ', err || (tunnel && tunnel.url))
   })
+})
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>${tunnelName} pi monitor </h1>
+    <p>System Memory: ${os.freemem()/1024/1024}Mb free out of ${os.totalmem()/1024/1024}Mb</p>
+    <p>Heap Memory: ${JSON.stringify(process.memoryUsage())}</p>
+    <p>System CPU load: ${os.loadavg()}</p>
+    <p>Process CPU: ${JSON.stringify(process.cpuUsage())}</p>
+    <p>System Uptime: ${os.uptime()}s</p>
+    <p>Process Uptime: ${process.uptime()}s</p>
+  `)
 })
