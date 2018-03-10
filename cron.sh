269a4d759be407c5b91c0718a99fdd8ad10e45ec
Stephen Clibbery <sdclibbery@googlemail.com>
2018-03-10 10:53:27 +0000

    logging

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [[ ! `netstat -tulpn | grep 8000 | grep node` ]]; then
-    invoke-rc.d sudo -u application node /home/application/pi-monitor/index.js
+    invoke-rc.d sudo -u application node /home/application/pi-monitor/index.js >> /home/application/pi-monitor.log
 fi
