5f4c593555f733f1b9396b707ef3e5bd976b3d2e
Stephen Clibbery <sdclibbery@googlemail.com>
2018-03-10 11:15:01 +0000

    no longer need to switch user

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [[ ! `netstat -tulpn 2>/dev/null | grep 8000 | grep node` ]]; then
-    sudo -u application node /home/application/pi-monitor/index.js >> /home/application/pi-monitor.log 2>&1
+    node /home/application/pi-monitor/index.js >> /home/application/pi-monitor.log 2>&1
 fi
