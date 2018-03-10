96ca3b999af0246431776100069f9433981828bf
Stephen Clibbery <sdclibbery@googlemail.com>
2018-03-10 11:00:02 +0000

    fix cron script

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [[ ! `netstat -tulpn | grep 8000 | grep node` ]]; then
-    invoke-rc.d sudo -u application node /home/application/pi-monitor/index.js >> /home/application/pi-monitor.log
+    sudo -u application node /home/application/pi-monitor/index.js >> /home/application/pi-monitor.log 2>&1
 fi
