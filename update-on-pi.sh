1c9c7509c90091c6d1d1c4bd0e0a13e2541e984b
Stephen Clibbery <sdclibbery@googlemail.com>
2018-03-21 23:56:56 +0000

    fix update script

--- update-on-pi.sh
+++ update-on-pi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kill -9 `ps aux | grep 'node.*pi-monitor' | awk '{print $2}' | head -n1`
+kill -9 `ps aux | grep 'node index.js' | awk '{print $2}' | tail -n1`
 git pull
 npm install
-kill -9 `ps aux | grep 'node.*pi-monitor' | awk '{print $2}' | head -n1`
+kill -9 `ps aux | grep 'node index.js' | awk '{print $2}' | tail -n1`
