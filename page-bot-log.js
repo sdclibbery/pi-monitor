a4dc3cd18c34ef3b2bdb6bcdbf3ed465aff6ebc6
Stephen Clibbery <sdclibbery@googlemail.com>
2018-03-25 10:14:46 +0100

    read logs asynchronously

--- page-bot-log.js
+++ page-bot-log.js
@@ -1,9 +1,15 @@
 const frame =  require('./frame').apply
 const fs = require('fs')
+const util = require('util')
 
-exports.render = (req, res) => {
+exports.render = async (req, res, next) => {
+  const log = await util.promisify(fs.readFile)(`../tradr/${req.params.logFile}`)
   res.send(frame(`
     <h1>${req.params.logFile}</h1>
-    <pre>${fs.readFileSync(`../tradr/${req.params.logFile}`)}</pre>
+    <pre>${format(log)}</pre>
   `))
+}
+
+const format = (log) => {
+  return log
 }
