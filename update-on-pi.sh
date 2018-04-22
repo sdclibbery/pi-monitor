c3a8faf5466cca0ce94ad496bf837c7b610b0ea1
sdclibbery <sdclibbery@googlemail.com>
2018-04-22 17:56:21 +0100

    gentler shutdown

--- update-on-pi.sh
+++ update-on-pi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kill -9 `ps aux | grep 'node index.js' | grep -v 'grep' | awk '{print $2}' | head -n1`
+kill `ps aux | grep 'node index.js' | grep -v 'grep' | awk '{print $2}' | head -n1`
 git pull
 npm install
-kill -9 `ps aux | grep 'node index.js' | grep -v 'grep' | awk '{print $2}' | head -n1`
+kill `ps aux | grep 'node index.js' | grep -v 'grep' | awk '{print $2}' | head -n1`
