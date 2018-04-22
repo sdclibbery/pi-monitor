4ee5b3a22f556c65f89648c1057cc83eaf08d92a
sdclibbery <sdclibbery@googlemail.com>
2018-04-22 17:30:20 +0100

    better order page

--- page-orders.js
+++ page-orders.js
@@ -11,13 +11,20 @@ exports.render = async (req, res, next) => {
 }
 
 const formatOrders = (orders) => {
+  const dp2 = (x) => Number.parseFloat(x).toFixed(2)
+  const dp4 = (x) => Number.parseFloat(x).toFixed(4)
   return orders
     .map(o => {
+      const profit = (o.side == 'buy' ? 1 : 1) * (o.orderPrice/o.priceAtCreation - 1) * 100
+      const baseCurrency = o.product.split('-')[0]
+      const quoteCurrency = o.product.split('-')[1]
       return `<tr>
       <td>${o.exchange} ${o.product}</td>
-      <td>${o.status} ${o.side}</td>
-      <td>${o.amount} ${o.orderPrice} ${o.product}</td>
-      <td>created at ${o.created} at ${o.priceAtCreation}</td>
+      <td>${o.status} <b>${o.side}</b></td>
+      <td><b>${dp4(o.amount)} ${baseCurrency}</b></td>
+      <td><b>${dp4(o.orderPrice)} ${quoteCurrency}</b></td>
+      <td>created at ${o.created} at price ${dp4(o.priceAtCreation)} ${quoteCurrency}</td>
+      <td>Profit if filled: ${dp2(profit)}%</td>
       <td>${o.creator} for ${o.reason}</td>
       </tr>`
     }).join('\n')
