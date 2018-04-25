3f8e6bb1657dec65eaef7dea8b36fe0a664639b8
sdclibbery <sdclibbery@googlemail.com>
2018-04-25 22:58:59 +0100

    add date to logging

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ x Launch tradr server and restart if necessary
 x Logging for tradr server
 x Proxy trading pages to tradr server
  x Separate localtunnel for tradr if possible
-* Sort out controls
- * Relaunch tradr node server
- * Shutdown monitor for Relaunch
- * Shutdown Pi
+x Sort out controls
+ x Relaunch tradr node server
+ x Shutdown monitor for Relaunch
+ x Shutdown Pi
 * Graceful shutdown...
  https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
  * Trap SIGs
--- expose-server.js
+++ expose-server.js
@@ -5,18 +5,18 @@ exports.expose = (name, port) => {
   const host =  os.hostname().toLowerCase().replace(/[^a-z0-9]/g, '')
   const localtunnelSubdomain = `sdclibbery-${host}-${name}`
   const launch = () => {
-    console.log(`Launching ${name} tunnel`)
+    console.log(`${new Date()} Launching ${name} tunnel`)
     localtunnel(port, { subdomain: localtunnelSubdomain }, (err, tunnel) => {
       if (err) {
-        console.log(`${name} tunnel launch failed: `, err)
+        console.log(`${new Date()} ${name} tunnel launch failed: `, err)
       }
       if (tunnel) {
-        console.log(`${name} tunnel at: `, tunnel.url)
+        console.log(`${new Date()} ${name} tunnel at: `, tunnel.url)
         tunnel.on('error', (e) => {
-          console.error(`${name} tunnel error: `, e)
+          console.error(`${new Date()} ${name} tunnel error: `, e)
         })
         tunnel.on('close', () => {
-          console.error(`${name} tunnel closed; relaunching`)
+          console.error(`${new Date()} ${name} tunnel closed; relaunching`)
           launch()
         })
       }
