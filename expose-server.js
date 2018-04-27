e4133c85a03a3daf2d62904200425dc388fd838f
sdclibbery <sdclibbery@googlemail.com>
2018-04-27 23:47:41 +0100

    hopefully better local tunnel handling without retry delays

--- expose-server.js
+++ expose-server.js
@@ -8,18 +8,22 @@ exports.expose = (name, port) => {
     console.log(`${new Date()} Launching ${name} tunnel`)
     localtunnel(port, { subdomain: localtunnelSubdomain }, (err, tunnel) => {
       if (err) {
-        console.log(`${new Date()} ${name} tunnel launch failed: `, err)
-      }
-      if (tunnel) {
+        console.log(`${new Date()} ${name} tunnel launch failed; retrying in 30s: `, err)
+        setTimeout(launch, 30000)
+      } else if (tunnel) {
         console.log(`${new Date()} ${name} tunnel at: `, tunnel.url)
         tunnel.on('error', (e) => {
-          console.error(`${new Date()} ${name} tunnel error: `, e)
+          console.error(`${new Date()} ${name} tunnel error; closing to relaunch: `, e)
           tunnel.close()
         })
         tunnel.on('close', () => {
-          console.error(`${new Date()} ${name} tunnel closed; relaunching`)
-          launch()
+          console.error(`${new Date()} ${name} tunnel closed; relaunching in 30s`)
+          setTimeout(launch, 30000)
         })
+        if (!tunnel.url.includes(localtunnelSubdomain)) {
+          console.error(`${new Date()} ${name} tunnel subdomain wrong; closing to relaunch`)
+          tunnel.close()
+        }
       }
     })
   }
