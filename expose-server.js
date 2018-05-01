e7ac667519f475d07e476ce4b85af7f7db10fa69
sdclibbery <sdclibbery@googlemail.com>
2018-05-01 23:23:17 +0100

    switch to ngrok instead of localtunnel

--- expose-server.js
+++ expose-server.js
@@ -1,49 +1,24 @@
-const localtunnel = require('localtunnel')
+const ngrok = require('ngrok')
 const os =  require('os')
+const credentials =  require('./ngrok-credentials').key
 
 exports.expose = (name, port) => {
   const host =  os.hostname().toLowerCase().replace(/[^a-z0-9]/g, '')
-  const localtunnelSubdomain = `sdclibbery-${host}-${name}`
-  let closing = false
-  let launching = true
-  let tunnelToClose = null
-  const launch = () => {
-    if (!launching) { return }
-    launching = false
-    if (closing) { return }
-    console.log(`${new Date()} Launching ${name} tunnel`)
-    localtunnel(port, { subdomain: localtunnelSubdomain }, (err, tunnel) => {
-      if (err) {
-        console.log(`${new Date()} ${name} tunnel launch failed`, err)
-        if (tunnel) { tunnel.close() }
-        if (tunnelToClose) { tunnelToClose.close() }
-      } else if (tunnel) {
-        console.log(`${new Date()} ${name} tunnel at: `, tunnel.url)
-        tunnelToClose = tunnel
-        tunnel.on('error', (e) => {
-          console.error(`${new Date()} ${name} tunnel error; closing to relaunch: `, e)
-          tunnel.close()
-        })
-        tunnel.on('close', () => {
-          if (!launching) {
-            console.error(`${new Date()} ${name} tunnel closed; relaunching in 60s`)
-            launching = true
-            setTimeout(() => {
-              tunnelToClose = null
-              launch()
-            }, 60000)
-          }
-        })
-        if (!tunnel.url.includes(localtunnelSubdomain)) {
-          console.error(`${new Date()} ${name} tunnel subdomain wrong; closing to relaunch`)
-          tunnel.close()
-        }
-      }
-    })
-  }
-  launch()
+  const tunnelSubdomain = `sdclibbery-${host}-${name}`
+
+  ngrok.connect({
+    addr: port,
+    subdomain: tunnelSubdomain,
+    authtoken: credentials,
+    region: 'eu',
+  }).then(url => {
+    console.log(`${new Date()} ngrok tunnel launched at ${url}`)
+  }).catch(err => {
+    console.error(`${new Date()} ngrok launch error `, err)
+  })
+
   return () => {
-    closing = true
-    if (tunnelToClose) { tunnelToClose.close() }
+    ngrok.disconnect()
+    console.log(`${new Date()} ngrok disconnect`)
   }
 }
